0e3d457e130f029534aff50437db860166055009
Anthony Ebert <ebertac@qut.edu.au>
2016-05-16 17:24:48 +1000

    First

---
+++ CPUsimulator.R
@@ -0,0 +1,29 @@
+
+
+BinToDec <- function(x) 
+  sum(2^(which(rev(unlist(strsplit(as.character(x), "")) == 1))-1))
+
+Register1 <- rep(0,32)
+
+example <- paste("000000","00001","00010","00011","10100","100000",sep="")
+
+#See https://en.wikipedia.org/wiki/MIPS_instruction_set
+
+MachineCode <- function(Register,input){
+  require(magrittr)
+  
+  #Addition
+  if(substr(example,start=1,stop=6) == "000000"){
+    t <- substr(example,start=7,stop=11) %>% BinToDec
+    s <- substr(example,start=12,stop=16) %>% BinToDec
+    d <- substr(example,start=17,stop=21) %>% BinToDec
+    output <- Register[t] + Register[s]
+    return(list(position=d,number=output))
+  }
+}
+
+
+#Example
+Register1[1] <- 10
+Register1[2] <- 5
+MachineCode(Register=Register1,input=example)
